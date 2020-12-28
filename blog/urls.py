a4e224022def533b599aef98506fa3e1a8dd3352
Rob <strummerandmalice@gmail.com>
2020-12-28 17:52:43 -0600

    blog functionality

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 
 from . import views 
 urlpatterns = [
-    path('',views.allblogs, name='allblogs'),
-    path('<int:blog_id/>', views.detail, name='detail'),
+    path('', views.allblogs, name='allblogs'),
+    path('<int:blog_id>/', views.detail, name='detail'),
     
 
 ]
